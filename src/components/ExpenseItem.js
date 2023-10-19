f5fe5e1ea609f2932dd7f86a11e0e8b86f57153e
RahulSharma <rahulmlc2003@gmail.com>
2023-10-19 23:47:02 +0530

    task_8

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,18 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
+    const ExpenseDate = new Date(2021, 2, 28);
+    const ExpenseTitle = 'Car insurance';
+    const ExpenseAmount = 294.67;
+    const locationOfExpenditure = 'Jaipur';
   return (
-    <div>
-        <h2>Expense Items</h2>
-      <div>
-        <p>Food Rs 10</p>
-        <p>Petrol Rs 100</p>
-        <p>Movies Rs 200</p>
-      </div>
+    <div className='expense-item'>
+        <div>{ExpenseDate.toISOString()}</div>
+        <div className='expense-item__description'>
+            <h2>{ExpenseTitle}</h2>
+            <div className='expense-item__price'>${ExpenseAmount}</div>
+            <div className='expense-item__location'>{locationOfExpenditure}</div>
+        </div>
     </div>
   );
 }
