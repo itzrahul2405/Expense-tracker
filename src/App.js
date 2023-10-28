c0b9a45c28e3e416ac92c5dbad3f916204578ce7
RahulSharma <rahulmlc2003@gmail.com>
2023-10-28 18:13:48 +0530

    task 1

--- src/App.js
+++ src/App.js
@@ -1,57 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
 import ExpenseItem from './components/Expenses/ExpenseItem';
+import React, {useState} from 'react';
 
 const App = () => {
 
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     {date: new Date(2021, 2, 29), title: 'Car insurance', amount: 294.67, location: 'jaipur'},
     {date: new Date(2021, 4, 6), title: 'jewellery', amount: 2000, location: 'mumbai'},
     {date: new Date(2021, 2, 17), title: 'bike', amount: 3500, location: "delhi"},
     {date: new Date(2021, 8, 4), title: 'shopping', amount: 315.7, location: "paris"},
     {date: new Date(2021, 1, 16), title: 'gift', amount: 20, location: 'new-york'}
-  ]
+  ])
 
-
-//   return (
-//     <div >
-//       <h2>Let's Get Started!</h2>
-//       <ExpenseItem
-//         ExpenseDate = {expenses[0].date}
-//         ExpenseTitle = {expenses[0].title}
-//         ExpenseAmount = {expenses[0].amount}
-//         locationOfExpenditure = {expenses[0].location}
-//       ></ExpenseItem>           
-//       <ExpenseItem
-//         ExpenseDate = {expenses[1].date}
-//         ExpenseTitle = {expenses[1].title}
-//         ExpenseAmount = {expenses[1].amount}
-//         locationOfExpenditure = {expenses[1].location}
-//       ></ExpenseItem>  
-//       <ExpenseItem
-//         ExpenseDate = {expenses[2].date}
-//         ExpenseTitle = {expenses[2].title}
-//         ExpenseAmount = {expenses[2].amount}
-//         locationOfExpenditure = {expenses[2].location}
-//       ></ExpenseItem>  
-//       <ExpenseItem
-//         ExpenseDate = {expenses[3].date}
-//         ExpenseTitle = {expenses[3].title}
-//         ExpenseAmount = {expenses[3].amount}
-//         locationOfExpenditure = {expenses[3].location}
-//       ></ExpenseItem>  
-//       <ExpenseItem
-//         ExpenseDate = {expenses[4].date}
-//         ExpenseTitle = {expenses[4].title}
-//         ExpenseAmount = {expenses[4].amount}
-//         locationOfExpenditure = {expenses[4].location}
-//       ></ExpenseItem>  
-      
-//     </div>
-//   );
-// }
-
+  const deleteExpenseHandler = (title) => {
+     // console.log(props.expenses)
+     const filteredExpenses = expenses.filter((expense) => expense.title !== title);
+     setExpenses(filteredExpenses)
+     // console.log(filteredExpenses)
+  }
 
   return (
       <div>
@@ -63,6 +29,7 @@ const App = () => {
             ExpenseTitle = {exp.title}
             ExpenseAmount = {exp.amount}
             locationOfExpenditure = {exp.location}
+            onDelete = {() => deleteExpenseHandler(exp.title)}
           />
         ))
         }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,16 +2,27 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/card';
+import React from 'react';
 
 
 const ExpenseItem = (props) => {
+  /*  below line can be used in place of line 24 to 28 than we do not need to use props  */
+  // const { ExpenseTitle, ExpenseAmount, ExpenseDate, locationOfExpenditure, onDelete} = props;   
 
   return (
     <Card className='expense-item'>
         <ExpenseDate ExpenseDate={props.ExpenseDate}/>
         <ExpenseDetails ExpenseTitle={props.ExpenseTitle} ExpenseAmount={props.ExpenseAmount} locationOfExpenditure={props.locationOfExpenditure}/>
-
+        <button onClick = {props.onDelete} >DELETE</button>
     </Card>
+
+    // <div className='expense-item'>
+    //   <h3>{ExpenseTitle}</h3>
+    //   <p>Amount: {ExpenseAmount}</p>
+    //   <p>Date: {ExpenseDate.toDateString()}</p>
+    //   <p>Location: {locationOfExpenditure}</p>
+    //   <button onClick = {onDelete}>DELETE</button>
+    // </div>
   );
 }
 
