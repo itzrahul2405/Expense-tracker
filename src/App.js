eaefcaa473f128633635cf1e20477f946eb6f691
RahulSharma <rahulmlc2003@gmail.com>
2023-11-01 21:02:01 +0530

    added some conditional renderings

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ const App = () => {
     {id: '2', date: new Date(2021, 4, 6), title: 'jewellery', amount: 2000, location: 'mumbai'},
     {id: '3', date: new Date(2021, 2, 17), title: 'bike', amount: 3500, location: "delhi"},
     {id: '4', date: new Date(2022, 8, 4), title: 'shopping', amount: 315.7, location: "paris"},
-    {id: '5', date: new Date(2022, 1, 16), title: 'gift', amount: 20, location: 'new-york'},
+    {id: '5', date: new Date(2020, 1, 16), title: 'gift', amount: 20, location: 'new-york'},
     {id: '6', date: new Date(2020, 2, 29), title: 'toilet paper', amount: 4.169, location: 'madras'},
     {id: '7', date: new Date(2020, 2, 29), title: 'icecream', amount: 2.67, location: 'swm'},
-    {id: '8', date: new Date(2019, 2, 29), title: 'food', amount: 8.12, location: 'kolkata'}
+    {id: '8', date: new Date(2020, 2, 29), title: 'food', amount: 8.12, location: 'kolkata'}
   ])
 
   const deleteExpenseHandler = (id) => {
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,25 +11,103 @@ const Expenses = (props) => {
   }
 
   const filteredExpenses = props.items.filter((expense) => expense.date.getFullYear() == parseFloat(filteredYear))
-   
+
+  
+
+//     return (
+
+//       <div>
+//           <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+
+//           {/* this is first way to render conditional statement */}
+
+//           {/* {filteredExpenses.length === 0 ? (
+//             <p>No Expenses Found.</p>
+//           ) : (
+//             filteredExpenses.map((exp, index) => (
+//             <ExpenseItem
+//               key = {exp.id}
+//               ExpenseDate = {exp.date}
+//               ExpenseTitle = {exp.title}
+//               ExpenseAmount = {exp.amount}
+//               locationOfExpenditure = {exp.location}
+//               onDelete = {() => props.onDelete(exp.id)}
+//             />
+//           ))
+//           )
+//           } */}
+
+
+
+
+
+
+//           {/* this is second way to render conditional statement ( it checks condition and if it is true it render part after && )*/}
+
+//           {filteredExpenses.length === 0 && <p>No Expenses Found.</p> }
+//           {filteredExpenses.length > 0 && 
+//             filteredExpenses.map((exp, index) => (
+//             <ExpenseItem
+//               key = {exp.id}
+//               ExpenseDate = {exp.date}
+//               ExpenseTitle = {exp.title}
+//               ExpenseAmount = {exp.amount}
+//               locationOfExpenditure = {exp.location}
+//               onDelete = {() => props.onDelete(exp.id)}
+//             />
+//           ))
+//           } 
+//       </div>
+        
+//     )
+// }
+
+
+
+
+
+    // this is the third way to render conditional statement
+
+    let expensesContent = <p>No Expenses Found.</p>
+
+    if (filteredExpenses.length  > 0 ){
+      expensesContent = filteredExpenses.map((exp, index) => (
+      <ExpenseItem
+        key = {exp.id}
+        ExpenseDate = {exp.date}
+        ExpenseTitle = {exp.title}
+        ExpenseAmount = {exp.amount}
+        locationOfExpenditure = {exp.location}
+        onDelete = {() => props.onDelete(exp.id)}
+      />
+      ))
+
+      if (filteredExpenses.length === 1){
+        expensesContent = [...expensesContent , <p>Only single Expense here. Please add more...</p>]
+      }    
+    }
+
+    // if (filteredExpenses.length === 1){
+    //   expensesContent = [...expensesContent , <p>Only single Expense here. Please add more...</p>]
+    // }
+
+
     return (
 
       <div>
           <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-          {filteredExpenses.map((exp, index) => (
-            <ExpenseItem
-              key = {exp.id}
-              ExpenseDate = {exp.date}
-              ExpenseTitle = {exp.title}
-              ExpenseAmount = {exp.amount}
-              locationOfExpenditure = {exp.location}
-              onDelete = {() => props.onDelete(exp.id)}
-            />
-          ))
-          }
+          {expensesContent}    
       </div>
         
     )
 }
+
+
+
+
+
+
+
+
 
 export default Expenses;
